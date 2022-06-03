cf0e760f172c8968fbe24b6dac70f58fced29c17
Tony Erickson <te_24_5@hotmail.com>
2022-06-03 09:15:52 -0500

    Testing out git workflows

---
+++ pushandpull.js
@@ -0,0 +1,7 @@
+
+var customerFirstName = 'Tony';
+var streetAddress = '2612 Wilde St';
+let fullName = customerFirstName + ' Erickson';
+console.log(fullName);
+
+console.log('Hi, my name is ' + customerFirstName + ' and I live at ' + streetAddress + '.');
